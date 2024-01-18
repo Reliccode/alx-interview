369f8a3b70db7938a93a9c3c6d1476c763863cca
Reliccode <bryanmaritim@gmail.com>
2024-01-18 13:34:03 +0300

    fix: pycodestyle compliance

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,35 @@
+#!/usr/bin/env python3
+'''
+Min operations
+'''
+
+
 def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+
+    Parameters:
+    - n (int): The target number of H characters.
+
+    Returns:
+    - int: The minimum number of operations needed.
+    If n is impossible to achieve, returns 0.
+    """
+
     if n <= 0:
         return 0
-    
-    # Function to find prime factors of a number
+
     def prime_factors(num):
+        """
+        Finds the prime factors of a given number.
+
+        Parameters:
+        - num (int): The number for which prime factors are to be found.
+
+        Returns:
+        - list: List of prime factors.
+        """
         factors = []
         divisor = 2
         while divisor * divisor <= num:
@@ -14,12 +40,10 @@ def minOperations(n):
         if num > 1:
             factors.append(num)
         return factors
-    
-    # Find prime factors of n
+
     factors = prime_factors(n)
-    
-    # Return the sum of prime factors as the unconventional solution
     return sum(factors)
+
 
 # Testing the unconventional solution
 n = 4
