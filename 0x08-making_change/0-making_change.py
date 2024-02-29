5cdaedcb8eb0f3fb0a120f6f14eddc62fef551fe
Reliccode <bryanmaritim@gmail.com>
2024-02-29 22:23:09 +0300

    fix: sorting coins in descending order to improve efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
-"""
-Making change
-"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
     """
-    Calculate the fewest number of coins needed to meet the given amount total.
+    Returns the fewest number of coins needed to meet the total.
 
     Args:
-    - coins: list of ints representing values of coins in possession.
+    - coins: A list of integers representing the values of the coins
     - total: An integer representing the target total amount.
 
     Returns:
-    - fewest num of coins needed to meet total.
+    - The fewest number of coins needed to meet the total.
       If total cannot be met by any number of coins you have, return -1.
     """
-    # If the target total is less than 0, it cannot be met by any number of
-    # coins
-    if total < 0:
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
         return 0
-    # If the target total is 0, no coins are needed
-    if total == 0:
-        return 0
-    
-    #sort coins in descending order
-    coins.sort(reverse=True)
 
-    # Initialize an array to store the minimum number of coins needed for each
-    # amount from 1 to total
+    # Initialize an array to store the minimum number of coins
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     # Iterate through each coin denomination
     for coin in coins:
-        # Update dp[j] if using coin reduces the number of coins needed to make
-        # amount j
+        # Update dp[j] if using coin reduces the number of coins
         for j in range(coin, total + 1):
             dp[j] = min(dp[j], dp[j - coin] + 1)
 
-    # If dp[total] is still infinity, it means the total cannot be met by any
-    # number of coins
+    # If dp[total] is still infinity, means total cannot be met by any num of
+    # coins
     if dp[total] == float('inf'):
         return -1
     else:
         return dp[total]
-
-
-# Example usage
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))  # Output should be 7
-    print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output should be -1
