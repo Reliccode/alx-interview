313fbbb9180165036248f5118e97b54810d2db66
Reliccode <bryanmaritim@gmail.com>
2024-02-29 22:27:02 +0300

    fix: removing sort

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,26 +13,21 @@ def makeChange(coins, total):
 
     Returns:
     - The fewest number of coins needed to meet the total.
-      If total cannot be met by any number of coins you have, return -1.
+      If total cannot be met by any number of coins you have
     """
+    # If coins is empty or None, return -1 indicating invalid input
     if not coins or coins is None:
         return -1
+    # If total is 0 or less, no coins are needed
     if total <= 0:
         return 0
 
-    # Initialize an array to store the minimum number of coins
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Iterate through each coin denomination
+    change = 0  # Initialize the variable to count the number of coins used
+    coins = sorted(coins)[::-1]  # Sort coins in descending order
     for coin in coins:
-        # Update dp[j] if using coin reduces the number of coins
-        for j in range(coin, total + 1):
-            dp[j] = min(dp[j], dp[j - coin] + 1)
-
-    # If dp[total] is still infinity, means total cannot be met by any num of
-    # coins
-    if dp[total] == float('inf'):
-        return -1
-    else:
-        return dp[total]
+        while coin <= total:
+            total -= coin
+            change += 1  # Increment the count of coins used
+        if total == 0:  # If total becomes 0, return the count of coins used
+            return change
+    return -1  # If total cannot be met by any number of coins, return -1
